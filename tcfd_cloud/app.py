cd1512c6450c4e3abeaf194859f91362ed83fa87
depp-impact <49706139+depp-impact@users.noreply.github.com>
2022-09-16 09:04:04 +0900

    Add files via upload

--- tcfd_cloud/app.py
+++ tcfd_cloud/app.py
@@ -1,7 +1,56 @@
 #import login
 #import tcdfView
+import streamlit as st
 import tcdfViewAws
-tcdfViewAws.view('milize', 'milize')
-#tcdfView.view('milize', 'milize')
+import dbUtil as SQLUtil
+import traceback
 
-#login.login()
+mysql_db = SQLUtil.mysql_db()
+host=st.secrets["mysql"]["host"]
+schema=st.secrets["mysql"]["database"]
+userid=st.secrets["mysql"]["user"]
+passwd=st.secrets["mysql"]["password"]
+mydb = mysql_db.connect(host, userid, passwd, schema)
+
+
+def get_table_column():
+    sql = f"select COLUMN_NAME, COLUMN_COMMENT from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = 'tcfd' and TABLE_NAME = 'property_information'"
+    #st.text(sql)
+    try:
+        recs = mysql_db.fetch(mydb, sql)
+        property_columns = {}
+
+        for rec in recs:
+            comment = rec[1]
+            if('\n' in rec[1]):
+                comment = rec[1].split('\n')[0]
+            if('(' in rec[1]):
+                comment = rec[1].split('(')[0]
+            property_columns.update({rec[0]:comment.replace('\u3000',' ')})
+
+        output_columns = {}
+        sql = f"select COLUMN_NAME, COLUMN_COMMENT from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = 'tcfd' and TABLE_NAME = 'output_information'"
+        
+        #st.text(sql)
+        recs = mysql_db.fetch(mydb, sql)
+        for rec in recs:
+            comment = rec[1]
+            if('\n' in rec[1]):
+                comment = rec[1].split('\n')[0]
+            if('(' in rec[1]):
+                comment = rec[1].split('(')[0]
+            output_columns.update({rec[0]:comment.replace('\u3000',' ')})
+        return property_columns, output_columns
+    except:
+        st.text(traceback.format_exc)
+
+st.set_page_config(
+    page_title="milize tcfd viewer",
+    layout="wide",
+    initial_sidebar_state="expanded", 
+)
+if(mydb is None):
+    st.text('mydb is None')
+else:
+    property_columns, output_columns = get_table_column()
+    tcdfViewAws.view(st, mydb, property_columns, output_columns, 'milize', 'milize')
--- tcfd_cloud/tcdfViewAws.py
+++ tcfd_cloud/tcdfViewAws.py
@@ -1,6 +1,5 @@
 import traceback
 #from turtle import onclick
-import streamlit as st
 from streamlit_folium import st_folium      # streamlitでfoliumを使う
 import folium
 import json
@@ -12,34 +11,11 @@ from st_aggrid.shared import GridUpdateMode
 import pandas as pd
 import requests
 
-pref={ '01':'北海道','02':'青森県','03':'岩手県','04':'宮城県','05':'秋田県','06':'山形県','07':'福島県',
-       '08':'茨城県','09':'栃木県','10':'群馬県','11':'埼玉県','12':'千葉県','13':'東京都','14':'神奈川県',
-       '15':'新潟県','16':'富山県','17':'石川県','18':'福井県','19':'山梨県','20':'長野県','21':'岐阜県',
-       '22':'静岡県','23':'愛知県','24':'三重県','25':'滋賀県','26':'京都府','27':'大阪府','28':'兵庫県',
-       '29':'奈良県','30':'和歌山県','31':'鳥取県','32':'島根県','33':'岡山県','34':'広島県','35':'山口県',
-       '36':'徳島県','37':'香川県','38':'愛媛県','39':'高知県','40':'福岡県','41':'佐賀県','42':'長崎県',
-       '43':'熊本県','44':'大分県','45':'宮崎県','46':'鹿児島県','47':'沖縄県',
-       '81':'北海道開発局','82':'東北地方整備局','83':'関東地方整備局','84':'北陸地方整備局','85':'中部地方整備局',
-       '86':'近畿地方整備局','87':'中国地方整備局','88':'四国地方整備局','88':'九州地方整備局','89':'沖縄総合事務局',
-    }
-optpref = st.sidebar.selectbox('県名', pref.values())
-
-st.set_page_config(
-    page_title="milize tcfd viewer",
-    layout="wide",
-    initial_sidebar_state="expanded", 
-)
-mysql_db = SQLUtil.mysql_db()
-host=st.secrets["mysql"]["host"]
-schema=st.secrets["mysql"]["database"]
-userid=st.secrets["mysql"]["user"]
-passwd=st.secrets["mysql"]["password"]
 #st.text(host)
 #st.text(userid)
 #st.text(passwd)
 #st.text(schema)
 #st.text(f"user={userid},password={passwd},host={host}, database={schema}")
-mydb = mysql_db.connect(host, userid, passwd, schema)
 
 def show_map_data(inputdata):
     lat = inputdata[0]['緯度']
@@ -78,9 +54,9 @@ def show_map_data(inputdata):
     st_data = st_folium(map, width=1200, height=800)
     #st.components.v1.html(folium.Figure().add_child(map_).render(), height=500)
 
-def get_calculate_result(pid):
-    sql = f"select water_depth {output_columns['water_depth']}, river_name {output_columns['river_name']},"\
-                f"damage_rate {output_columns['damage_rate']}, damage_amount {output_columns['damage_amount']} "\
+def get_calculate_result(mydb, pid, outColumns):
+    sql = f"select water_depth {outColumns['water_depth']}, river_name {outColumns['river_name']},"\
+                f"damage_rate {outColumns['damage_rate']}, damage_amount {outColumns['damage_amount']} "\
                 f"from tcfd.output_information where property_id = '{pid}';"
 
     df = pd.read_sql(sql, mydb, coerce_float=True)
@@ -93,17 +69,15 @@ def get_calculate_result(pid):
                 gridOptions=gridOptions, height=85)
 
 #def get_property(pref, property_columns, output_columns):
-def get_property(pref):
+def get_property(st, mydb, inColumns, outColumns, pref):
     #property_type = 1:マンション 2:アパート 3:戸建て 4:企業 5:農地等
-    sql = f"select property_id {property_columns['property_id']}, contract_number {property_columns['contract_number']},"\
-                f"property_type {property_columns['property_type']}, loan_amount {property_columns['loan_amount']},"\
-                f"loan_balance {property_columns['loan_balance']}, DATE_FORMAT(loan_end_date,'%Y/%m/%d') {property_columns['loan_end_date']},"\
-                f"address {property_columns['address']}, latitude {property_columns['latitude']}, longitude {property_columns['longitude']},"\
-                f"assessed_land_amount {property_columns['assessed_land_amount']}, assessed_building_amount {property_columns['assessed_building_amount']},"\
-                f"assessed_apartment_amount {property_columns['assessed_apartment_amount']} "\
+    sql = f"select property_id {inColumns['property_id']}, contract_number {inColumns['contract_number']},"\
+                f"property_type {inColumns['property_type']}, loan_amount {inColumns['loan_amount']},"\
+                f"loan_balance {inColumns['loan_balance']}, DATE_FORMAT(loan_end_date,'%Y/%m/%d') {inColumns['loan_end_date']},"\
+                f"address {inColumns['address']}, latitude {inColumns['latitude']}, longitude {inColumns['longitude']},"\
+                f"assessed_land_amount {inColumns['assessed_land_amount']}, assessed_building_amount {inColumns['assessed_building_amount']},"\
+                f"assessed_apartment_amount {inColumns['assessed_apartment_amount']} "\
             f"from tcfd.property_information where address like '{pref}%' and (latitude is not null and longitude is not null);"
-    #print(sql)
-    #df = pd.read_sql(sql, mydb, coerce_float=True, index_col=property_columns['property_id'])
     df = pd.read_sql(sql, mydb, coerce_float=True)
     gb = GridOptionsBuilder.from_dataframe(df)
     gb.configure_selection(use_checkbox=True)
@@ -122,7 +96,7 @@ def get_property(pref):
         #inLabel1, inLabel2, inLabel3, inLabel4 = st.columns(4)
         #st.text()
         #st.write(data["selected_rows"])
-        calc = get_calculate_result(data["selected_rows"][0]['物件ID'])
+        calc = get_calculate_result(data[mydb, "selected_rows"][0]['物件ID'], outColumns)
         lat = data["selected_rows"][0]['緯度']
         lon = data["selected_rows"][0]['経度']
 
@@ -144,50 +118,19 @@ def get_property(pref):
     #AgGrid(df, fit_columns_on_grid_load=True,width=1500, height=800)
     #st.table(df,height=800)
 
-def get_table_column():
-    sql = f"select COLUMN_NAME, COLUMN_COMMENT from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = 'tcfd' and TABLE_NAME = 'property_information'"
-    #st.text(sql)
-    try:
-        recs = mysql_db.fetch(mydb, sql)
-        property_columns = {}
 
-        for rec in recs:
-            comment = rec[1]
-            if('\n' in rec[1]):
-                comment = rec[1].split('\n')[0]
-            if('(' in rec[1]):
-                comment = rec[1].split('(')[0]
-            property_columns.update({rec[0]:comment.replace('\u3000',' ')})
+def view(st, mydb, inColumns, outColumns, userid, passwd):
+    pref={ '01':'北海道','02':'青森県','03':'岩手県','04':'宮城県','05':'秋田県','06':'山形県','07':'福島県',
+        '08':'茨城県','09':'栃木県','10':'群馬県','11':'埼玉県','12':'千葉県','13':'東京都','14':'神奈川県',
+        '15':'新潟県','16':'富山県','17':'石川県','18':'福井県','19':'山梨県','20':'長野県','21':'岐阜県',
+        '22':'静岡県','23':'愛知県','24':'三重県','25':'滋賀県','26':'京都府','27':'大阪府','28':'兵庫県',
+        '29':'奈良県','30':'和歌山県','31':'鳥取県','32':'島根県','33':'岡山県','34':'広島県','35':'山口県',
+        '36':'徳島県','37':'香川県','38':'愛媛県','39':'高知県','40':'福岡県','41':'佐賀県','42':'長崎県',
+        '43':'熊本県','44':'大分県','45':'宮崎県','46':'鹿児島県','47':'沖縄県',
+        '81':'北海道開発局','82':'東北地方整備局','83':'関東地方整備局','84':'北陸地方整備局','85':'中部地方整備局',
+        '86':'近畿地方整備局','87':'中国地方整備局','88':'四国地方整備局','88':'九州地方整備局','89':'沖縄総合事務局',
+        }
+    optpref = st.sidebar.selectbox('県名', pref.values())
+    get_property(st, mydb, inColumns, outColumns, optpref)
 
-        output_columns = {}
-        sql = f"select COLUMN_NAME, COLUMN_COMMENT from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA = 'tcfd' and TABLE_NAME = 'output_information'"
-        
-        #st.text(sql)
-        recs = mysql_db.fetch(mydb, sql)
-        for rec in recs:
-            comment = rec[1]
-            if('\n' in rec[1]):
-                comment = rec[1].split('\n')[0]
-            if('(' in rec[1]):
-                comment = rec[1].split('(')[0]
-            output_columns.update({rec[0]:comment.replace('\u3000',' ')})
-        return property_columns, output_columns
-    except:
-        st.text(traceback.format_exc)
-
-def view(userid, passwd):
-    if(mydb is None):
-        st.text('mydb is None')
-        return
-    #st.text('mydb is connect!!')
-    #st.set_page_config(layout="wide")
-    #print('view:',userid, passwd)
-    #mydb = mysql_db.connect('market')
-    #dataTypes = get_schema_tables('market')
-    #print(property_columns, output_columns)
-    #st.title("---milize tcfd output viewer---")
-    #get_property(optpref, property_columns, output_columns)
-    get_property(optpref)
-
-property_columns, output_columns = get_table_column()
 
